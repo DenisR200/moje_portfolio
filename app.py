1a4ccd0869329191ff39de4cd9159361da132b14
DenisR200 <24320@student.vitkovickastredni.cz>
2025-10-08 09:40:10 +0200

    commit_falsk

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.htm')
+    nazev_webu =  "Denisovo Portfolio"
+    popis = "Ukázka projektů, výuka s AI, a testování Jitsi2"
+    technologie =  ["Flask", "Python", "HTML", "CSS", "Jitsi2", "Copilot"]
+
+    return render_template('index.htm', nazev_webu = nazev_webu)
 
 if __name__ == '__main__':
     app.run(debug=True)
