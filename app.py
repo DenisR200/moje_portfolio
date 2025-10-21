61e76ba090514f020b1ff943fc908666ac94901d
DenisR200 <24320@student.vitkovickastredni.cz>
2025-10-21 11:06:42 +0200

    §ůůů

--- app.py
+++ app.py
@@ -1,51 +1,11 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
 
-users = {
-    1: {
-        "jmeno": "Jan",
-        "prijmeni": "Novák",
-        "email": "jan.novak@example.com",
-        "telefon": "+420123456789",
-        "kancelar": "101A"
-    },
-    2: {
-        "jmeno": "Petra",
-        "prijmeni": "Svobodová",
-        "email": "petra.svobodova@example.com",
-        "telefon": "+420987654321",
-        "kancelar": "102B"
-    },
-    3: {
-        "jmeno": "Martin",
-        "prijmeni": "Kapcala",
-        "email": "martin.kapcala@example.com",
-        "telefon": "+420456789123",
-        "kancelar": "103C"
-    },
-    4: {
-        "jmeno": "Lucie",
-        "prijmeni": "Dvořáková",
-        "email": "lucie.dvorakova@example.com",
-        "telefon": "+420321654987",
-        "kancelar": "104D"
-    },
-    5: {
-        "jmeno": "Tomáš",
-        "prijmeni": "Havlásek",
-        "email": "tomas.havlasek@example.com",
-        "telefon": "+420789123456",
-        "kancelar": "105E"
-    }
-}
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    nazev_webu =  "Denisovo Portfolio"
-    titulek_webu= "Svět, který jsem si stvořil, je výsledkem našeho myšlení. Nelze jej měnit."
-    popis = "Ukázka projektů, výuka s AI, a testování Jitsi2"
-    technologie =  ["Flask", "Python", "HTML", "CSS", "Jitsi2", "Copilot"]
+
 
     return render_template('index.htm', nazev_webu = nazev_webu, titulek_webu = titulek_webu, technologie = technologie)
 
