30f5982310c2fbe3bffe28c6bc4351e3edccfda0
DenisR200 <24320@student.vitkovickastredni.cz>
2025-10-23 08:45:14 +0200

    commit_dalsi

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from data import users, nazev_webu, popis, technologie, titulek_webu
-from generator import generuj_cisla
+from generator import cislo
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+@app.route('/', methods=["GET", "POST"])
+def home():
+    email = None
 
+    if request.method == "GET":
+        email = request.form.get("email")
 
-    return render_template('index.htm', nazev_webu = nazev_webu, titulek_webu = titulek_webu, technologie = technologie)
+    return render_template('index.htm', nazev_webu = nazev_webu, titulek_webu = titulek_webu, technologie = technologie, email = email)
 
 
 
@@ -31,8 +34,6 @@ if __name__ == "__main__":
 
         @app.route('/generator')
         def generator():
-            gen = generuj_cisla ()
-            cislo = list(gen)
             return render_template("base.htm", cislo = cislo)
 
         if __name__ == '__main__':
